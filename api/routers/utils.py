7cbce05e11f847ca51cef9ec6d28883360e5ac2a
Ten <03822007sasha@gmail.com>
2025-03-19 17:41:20 +0300

    The code inside the module is commented out

--- api/routers/utils.py
+++ api/routers/utils.py
@@ -1,14 +1,29 @@
 from pathlib import Path
+from typing import Generator, Any
 
-
-def iter_file(file_path: Path | str, start: int, end: int, chunk_size: int = 1024):
-    """Читаем файл частями для стриминга"""
-    with open(file_path, "rb") as f:
+def iter_file(
+        file: bytes | Path | str,
+        start: int,
+        end: int,
+        chunk_size: int = 1024
+) -> Generator[bytes, Any, None]:
+    """
+    Функция читает файл частями для стриминга
+    :param file: Путь до файла или байты файла
+    :param start: Откуда начать читать файл по байтам
+    :param end: Где закончить читать файл
+    :param chunk_size: Размер чанка который
+        мы будем считывать за раз.
+    :return: Возвращает генератор или None
+    """
+    with open(file, "rb") as f:
         f.seek(start)
+
         while start < end:
             chunk = f.read(min(chunk_size, end - start))
             if not chunk:
                 break
+
             start += len(chunk)
             yield chunk
 
