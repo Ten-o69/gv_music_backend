a29c471b534413348633dae0922b3c9f94db123a
Ten <03822007sasha@gmail.com>
2025-03-19 09:45:39 +0300

    Adding a module with pydantic schemes

---
+++ schemas/music.py
@@ -0,0 +1,22 @@
+from typing import Optional, List
+
+from pydantic import BaseModel, UUID4, HttpUrl
+
+
+class TrackSchema(BaseModel):
+    id: UUID4
+    title: Optional[str] = None
+    artist: Optional[str] = None
+    url: HttpUrl
+    duration: int
+
+    class Config:
+        orm_mode = True
+
+
+class TrackListResponse(BaseModel):
+    total: int
+    offset: int
+    limit: int
+    tracks: Optional[List[TrackSchema]] = None
+    next_offset: Optional[int] = None
