c4177045a4ca933eb8292b26ea63b9ec924a9dfe
Ten <03822007sasha@gmail.com>
2025-03-21 04:24:51 +0300

    The code inside the common.helpers module is commented out

--- common/helpers.py
+++ common/helpers.py
@@ -6,4 +6,11 @@ from .constants import (
 
 
 def get_relative_path(path: Path) -> str:
+    """
+    Функция для получения относительного пути
+    в универсальном формате
+    :param path: Путь который нужно преобразовать.
+    :return: Возвращает строку с преобразованным путём
+    """
+
     return path.relative_to(BASE_DIR).as_posix()
