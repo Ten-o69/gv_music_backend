a61e3246efb48fd3be5f64c7ef3aec63183520dc
Ten-o69 <03822007sasha@gmail.com>
2025-04-15 13:12:29 +0300

    Removed validation of the 'DATABASE_URL' variable. Now the 'DATABASE_URL' variable validation is handled by the 'common.constants' module

--- database/config.py
+++ database/config.py
@@ -1,20 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from ten_utils.log import Logger
 
 from common.constants import (
     DATABASE_URL,
+    DATABASE_LOG,
 )
 
 
-logger = Logger(__name__, level=3)
-
-
-if DATABASE_URL:
-    engine = create_engine(DATABASE_URL, echo=True)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    Base = declarative_base()
-
-else:
-    logger.error("DATABASE_URL environment variable is not set")
-    exit(1)
+engine = create_engine(DATABASE_URL, echo=DATABASE_LOG)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
