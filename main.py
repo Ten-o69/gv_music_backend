2c9b11938a00865633b158073072fe1f65491acf
Ten-o69 <03822007sasha@gmail.com>
2025-04-15 11:29:00 +0300

    Now the server will start in the mode of automatic reboot after making any changes

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import uvicorn
 
-from api.app import app
-
 
 def run_server():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app="api.app:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
