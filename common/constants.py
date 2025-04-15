433450458b0abd961457970725d78f0646c0b03a
Ten-o69 <03822007sasha@gmail.com>
2025-04-15 12:52:36 +0300

    Getting environment variables is now done using the 'env_loader' tool from the 'ten_utils' library. Also added new environment variables for more fine-tuning of CORS inside the API

--- common/constants.py
+++ common/constants.py
@@ -1,48 +1,39 @@
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv
-from ten_utils.log import Logger
+from ten_utils.log import LoggerConfig
+from ten_utils.env_loader import EnvLoader
 
 
 # base
-env_mode = os.getenv("ENV", "dev")
-logger = Logger(__name__, level=3)
-load_dotenv(f".env.{env_mode}")
+BASE_DIR = Path(__file__).parent.parent
 
+# env
+ENV_MODE = os.getenv("ENV", "dev")
+env_loader = EnvLoader(BASE_DIR / f".env.{ENV_MODE}")
 
 # path/dir
-BASE_DIR = Path(__file__).parent.parent
-DIR_DATA = os.getenv("DIR_DATA", None)
-if DIR_DATA:
-    DIR_DATA = Path(DIR_DATA)
-
-else:
-    logger.critical("DIR_DATA environment variable is not set")
-
+DIR_DATA = env_loader.load("DIR_DATA", Path)
 DIR_MUSIC = DIR_DATA / "music"
 DIR_MUSIC_COVER = DIR_DATA / "music_cover"
 DIR_STATIC = BASE_DIR / "static"
 
 # url
-URL_MUSIC = "music/"
-URL_MUSIC_COVER = "music_cover/"
+URL_MUSIC = "music_tracks/"
+URL_MUSIC_COVER = "music_track_covers/"
 URL_MUSIC_STREAM = "api/v1/tracks/"
 
 # database
-DATABASE_URL = os.getenv("DATABASE_URL", None)
+DATABASE_URL = env_loader.load("DATABASE_URL", str)
+DATABASE_LOG: bool = env_loader.load("DATABASE_LOG", bool)
 
 # api
-API_ALLOW_HOSTS = os.getenv("ALLOW_HOSTS", None)
-if API_ALLOW_HOSTS:
-    API_ALLOW_HOSTS = API_ALLOW_HOSTS.split(",")
+API_ALLOW_HOSTS = env_loader.load("API_ALLOW_HOSTS", tuple)
+API_CORS_ALLOW_ORIGINS = env_loader.load("API_CORS_ALLOW_ORIGINS", tuple)
+API_CORS_ALLOW_METHODS = env_loader.load("API_CORS_ALLOW_METHODS", tuple)
+API_CORS_ALLOW_CREDENTIALS = env_loader.load("API_CORS_ALLOW_CREDENTIALS", bool)
 
-else:
-    logger.critical("ALLOW_HOSTS environment variable is not set")
-
-API_CORS_HOSTS = os.getenv("API_CORS_HOSTS", None)
-if API_CORS_HOSTS:
-    API_CORS_HOSTS = API_CORS_HOSTS.split(",")
-
-else:
-    logger.critical("API_CORS_HOSTS environment variable is not set")
+# log
+LoggerConfig().set_default_level_log(
+    env_loader.load("LOG_LEVEL", int)
+)
